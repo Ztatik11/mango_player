2c72d5bf0f33460f40a897a2bf1a29d5862e92fd
Ztatik11 <99404495+Ztatik11@users.noreply.github.com>
2023-02-22 21:32:38 +0100

    0.11

--- src/App.js
+++ src/App.js
@@ -4,17 +4,6 @@ import Music_player from './components/music_player'
 import styles from './styles/InsertarFrutas'
 import generalStyle from './styles/music_player'
 
-  const app = express();
-  const port = process.env.PORT || 5000;
-
-  app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
-  });
-
-  app.listen(port, () => {
-   console.log(`El servidor está corriendo en el puerto ${port}`);
-  });
-
 const App = () => {
   
   return (
